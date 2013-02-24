a7e5ac7328603b15f5e60c69cb6bdbc90578d0e0
Gael-H <following@live.fr>
2013-02-24 14:53:08 +0100

    ajout fichiers manquant

---
+++ AppliIUTSDdeLorraine/GEII_ViewController.m
@@ -0,0 +1,70 @@
+//
+//  IUT_ViewController.m
+//  AppliIUTSDdeLorraine
+//
+//  Created by Gaël Hellé on 14/02/13.
+//  Copyright (c) 2013 Damien Bannerot. All rights reserved.
+//
+
+#import "GEII_ViewController.h"
+
+@interface GEII_ViewController ()
+
+@end
+
+@implementation GEII_ViewController
+
+@synthesize myDocView;
+
+
+
+
+
+
+
+- (void)viewDidLoad
+{
+    NSURL * resourcePathURL = [[NSBundle mainBundle] resourceURL];
+    if(resourcePathURL)
+    {
+        NSURL * urlToLoad = [resourcePathURL
+                             URLByAppendingPathComponent: @"geii.html"];
+        if(urlToLoad)
+        {
+            NSURLRequest * req = [NSURLRequest requestWithURL: urlToLoad];
+            [myDocView loadRequest: req];
+        }
+    }
+}
+
+
+
+
+- (BOOL)webView:(UIWebView *)webView
+shouldStartLoadWithRequest:(NSURLRequest *)request
+ navigationType:(UIWebViewNavigationType)navigationType {
+    //if (navigationType == UIWebViewNavigationTypeFormSubmitted) {
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        NSURL *url = [request URL];
+        if (![[url scheme] hasPrefix:@"file"]) {
+            [[UIApplication sharedApplication] openURL:url];
+            return NO;
+        }
+    }
+    return YES;
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] ==
+        UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation !=
+                UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+
+@end
---
+++ AppliIUTSDdeLorraine/INFO_ViewController.h
@@ -0,0 +1,22 @@
+//
+//  IUT_ViewController.h
+//  AppliIUTSDdeLorraine
+//
+//  Created by Gaël Hellé on 14/02/13.
+//  Copyright (c) 2013 Damien Bannerot. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface INFO_ViewController : UIViewController<UIWebViewDelegate>{
+    IBOutlet UIWebView *myDocView;
+    
+}
+@property (nonatomic, retain) IBOutlet UIWebView *myDocView;
+
+
+@end
+
+
+
